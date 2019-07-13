b14334b44a6286140894ebf6c05b096a16de7d45
k3d3 <k3d3>
2019-07-13 11:24:46 -0400

    Rustfmt

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,29 @@
-use structopt::StructOpt;
 use std::path::PathBuf;
+use structopt::StructOpt;
 
 /// Mirror rustup and crates.io repositories, for offline Rust and cargo usage.
 #[derive(Debug, StructOpt)]
-#[structopt(name="panamax")]
+#[structopt(name = "panamax")]
 struct Opt {
     /// Output directory
     #[structopt(parse(from_os_str))]
     out: PathBuf,
 
     /// Only mirror the rustup files
-    #[structopt(short, long="rustup")]
+    #[structopt(short, long = "rustup")]
     rustup_only: bool,
 
     /// Only mirror crates.io
-    #[structopt(short, long="crates")]
+    #[structopt(short, long = "crates")]
     crates_only: bool,
 
     /// Number of threads to download with
-    #[structopt(short, long="threads")]
+    #[structopt(short, long = "threads")]
     threads: Option<usize>,
 
     /// Rewrite the download URL in crates.io-index (should point to the /crates directory)
-    #[structopt(short="u", long="url")]
-    rewrite_url: Option<String>
+    #[structopt(short = "u", long = "url")]
+    rewrite_url: Option<String>,
 }
 
 fn main() {
