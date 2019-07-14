762d1c2c2cb17adbc06ee2a4c1d34425937cd076
k3d3 <k@3d3.ca>
2019-07-14 17:39:39 -0400

    Update skeleton

--- src/crates.rs
+++ src/crates.rs
@@ -4,12 +4,14 @@ use console::style;
 pub fn sync(mirror: &MirrorSection, crates: &CratesSection) {
     eprintln!("{}", style("Syncing Crates repositories...").bold());
 
-    eprintln!("{} Syncing crates.io-index repository...", style("[1/2]").bold());
+    eprintln!("{} Syncing crates.io-index repository...", style("[1/3]").bold());
 
-    eprintln!("{} Syncing crates...", style("[2/2]").bold());
+    eprintln!("{} Syncing crates...", style("[2/3]").bold());
 
     if let Some(base_url) = &mirror.base_url {
-        eprintln!("      Rewriting URL in crates.io-index config.json...");
+        eprintln!("{} Rewriting config.json URL in index...", style("[3/3]").bold());
+    } else {
+
     }
 
     eprintln!("{}", style("Syncing Crates repositories complete!").bold());
