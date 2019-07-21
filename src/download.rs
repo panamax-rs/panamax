fd6713a59302df07df8ea064ca328ec811b1e506
k3d3 <k@3d3.ca>
2019-07-21 15:53:33 -0400

    Add DownloadError error type, as well as download plans

--- src/download.rs
+++ src/download.rs
@@ -4,14 +4,33 @@ use std::fs::File;
 use sha2::{Digest, Sha256};
 use std::io::{Read, Write, ErrorKind};
 
+// General download strategy:
+// 1: Download the sha256 file (or acquire the needed sha256 for a file)
+// 2: Download the file to <name>.part and check sha256 as it downloads
+// 3: Only when file is fully download and sha256 verified, move file to <name>
+// If the <name> file already exists, don't bother downloading it again
+// If downloading fails (sha256 doesn't match), retry downloading up to 5 times.
+// If retries run out, keep note of the failure somewhere.
+
+quick_error! {
+    #[derive(Debug)]
+    pub enum DownloadError {
+        Io(err: io::Error) {
+            from()
+        }
+        Download(err: reqwest::Error) {
+            from()
+        }
+    }
+}
+
 /// Download a URL and return it as a string.
-pub fn download_string(from: &str) -> String {
-    // TODO: Error handling
-    reqwest::get(from).unwrap().text().unwrap()
+pub fn download_string(from: &str) -> Result<String, DownloadError> {
+    Ok(reqwest::get(from)?.text()?)
 }
 
 /// Write a string to a file, creating directories if needed.
-pub fn write_file_create_dir(path: &Path, contents: &str) -> io::Result<()> {
+pub fn write_file_create_dir(path: &Path, contents: &str) -> Result<(), DownloadError> {
     let mut res = fs::write(path, contents);
 
     if let Err(e) = &res {
@@ -23,11 +42,11 @@ pub fn write_file_create_dir(path: &Path, contents: &str) -> io::Result<()> {
         }
     }
 
-    res
+    Ok(res?)
 }
 
 /// Create a file, creating directories if needed.
-pub fn create_file_create_dir(path: &Path) -> io::Result<File> {
+pub fn create_file_create_dir(path: &Path) -> Result<File, DownloadError> {
     let mut file_res = File::create(path);
     if let Err(e) = &file_res {
         if e.kind() == io::ErrorKind::NotFound {
@@ -38,23 +57,22 @@ pub fn create_file_create_dir(path: &Path) -> io::Result<File> {
         }
     }
 
-    file_res
+    Ok(file_res?)
 }
 
 /// Download a file to a path, creating directories if needed.
-pub fn download_and_create_dir(from: &str, to: &Path) -> io::Result<()> {
-    // TODO: Error handling
-    let mut http_res = reqwest::get(from).unwrap();
+pub fn download_and_create_dir(from: &str, to: &Path) -> Result<(), DownloadError> {
+    let mut http_res = reqwest::get(from)?;
 
     let mut f = create_file_create_dir(to)?;
 
-    http_res.copy_to(&mut f).unwrap();
+    http_res.copy_to(&mut f)?;
 
     Ok(())
 }
 
 /// Get the (lowercase hex) sha256 hash of a file.
-pub fn file_sha256(path: &Path) -> io::Result<String> {
+pub fn file_sha256(path: &Path) -> Result<String, DownloadError> {
     let mut file = File::open(path)?;
     let mut sha256 = Sha256::new();
     fast_copy(&mut file, &mut sha256)?;
@@ -62,7 +80,7 @@ pub fn file_sha256(path: &Path) -> io::Result<String> {
 }
 
 /// If a file doesn't match a provided sha256, download a url to a path.
-pub fn download_with_sha256_str_verify(url: &str, path: &Path, remote_sha256: &str) -> io::Result<()> {
+pub fn download_with_sha256_str_verify(url: &str, path: &Path, remote_sha256: &str) -> Result<(), DownloadError> {
     let do_download = if let Ok(local_file_sha256) = file_sha256(path) {
         remote_sha256 != local_file_sha256
     } else {
@@ -77,7 +95,7 @@ pub fn download_with_sha256_str_verify(url: &str, path: &Path, remote_sha256: &s
 }
 
 /// If an accompanying .sha256 file doesn't match or exist, download a url to a path.
-pub fn download_with_sha256_verify(url: &str, path: &Path) -> io::Result<()> {
+pub fn download_with_sha256_verify(url: &str, path: &Path) -> Result<(), DownloadError> {
     let sha256_url = format!("{}.sha256", url);
     let sha256_path = {
         let mut new_path = path.as_os_str().to_os_string();
@@ -85,7 +103,7 @@ pub fn download_with_sha256_verify(url: &str, path: &Path) -> io::Result<()> {
         PathBuf::from(new_path)
     };
 
-    let remote_sha256 = download_string(&sha256_url);
+    let remote_sha256 = download_string(&sha256_url)?;
 
     let do_download = if let Ok(local_sha256) = fs::read_to_string(&sha256_path) {
         if local_sha256 == remote_sha256 {
