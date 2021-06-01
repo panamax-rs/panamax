9fc434033cc43e1f919713bacbc42efa18bae25f
k3d3 <k@3d3.ca>
2021-06-01 17:22:56 -0400

    Run rustfmt on change

--- src/rustup.rs
+++ src/rustup.rs
@@ -382,7 +382,9 @@ pub fn rustup_download_list(
                 pkg.target
                     .into_iter()
                     .filter(|(name, _)| {
-                        platforms.unix.contains(&name) || platforms.windows.contains(&name) || name == "*"
+                        platforms.unix.contains(&name)
+                            || platforms.windows.contains(&name)
+                            || name == "*"
                     })
                     .flat_map(|(_, target)| -> Vec<(String, String)> {
                         target
