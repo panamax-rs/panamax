b7b91e9c5737bd5b03b949a7ab9fa922bcdbd5ef
k3d3 <k@3d3.ca>
2021-06-26 19:05:34 -0400

    Fix up css, finish up backend stuff.
    Everything works!

--- src/serve.rs
+++ src/serve.rs
@@ -1,44 +1,73 @@
-use std::{
-    collections::HashMap,
-    io,
-    net::SocketAddr,
-    path::{Path, PathBuf},
-    process::Stdio,
-};
+use std::{collections::HashMap, io, net::SocketAddr, path::PathBuf, process::Stdio};
 
 use askama::Template;
 use bytes::BytesMut;
+use futures_util::stream::TryStreamExt;
 use include_dir::{include_dir, Dir};
+use thiserror::Error;
 use tokio::{
+    fs::File,
     io::{AsyncBufReadExt, AsyncReadExt, AsyncWriteExt, BufReader},
     process::{ChildStdout, Command},
 };
 use tokio_stream::StreamExt;
+use tokio_util::codec::{BytesCodec, FramedRead};
 use warp::{
+    host::Authority,
     http,
     hyper::{body::Sender, Body, Response},
     path::Tail,
+    reject::Reject,
     Filter, Rejection, Stream,
 };
 
 #[derive(Template)]
 #[template(path = "index.html")]
 struct IndexTemplate {
-    platforms: Vec<String>,
+    platforms: Vec<(bool, String)>,
     host: String,
 }
 
 const STATIC_DIR: Dir = include_dir!("static");
 
+#[derive(Error, Debug)]
+pub enum ServeError {
+    #[error("IO error: {0}")]
+    Io(#[from] io::Error),
+    #[error("Hyper error: {0}")]
+    Hyper(#[from] warp::hyper::Error),
+    #[error("Warp HTTP error: {0}")]
+    Warp(#[from] warp::http::Error),
+    #[error("{0}")]
+    Other(String),
+}
+
+impl Reject for ServeError {}
+
 pub async fn serve(path: PathBuf, socket_addr: SocketAddr, tls_paths: Option<(PathBuf, PathBuf)>) {
-    let host = "http://panamax.somewhere".to_string();
-
-    let platforms = get_rustup_platforms(&path).await.unwrap();
-
-    let index = warp::path::end().map(move || IndexTemplate {
-        platforms: platforms.clone(),
-        host: host.clone(),
-    });
+    let index_path = path.clone();
+    let is_tls = tls_paths.is_some();
+    let index = warp::path::end().and(warp::host::optional()).and_then(
+        move |authority: Option<Authority>| {
+            let mirror_path = index_path.clone();
+            let protocol = if is_tls { "https://" } else { "http://" };
+            async move {
+                get_rustup_platforms(mirror_path)
+                    .await
+                    .map(|platforms| IndexTemplate {
+                        platforms: platforms,
+                        host: authority
+                            .map(|a| format!("{}{}", protocol, a.as_str()))
+                            .unwrap_or_else(|| "http://panamax.internal".to_string()),
+                    })
+                    .map_err(|_| {
+                        warp::reject::custom(ServeError::Other(
+                            "Could not retrieve rustup platforms.".to_string(),
+                        ))
+                    })
+            }
+        },
+    );
 
     let static_dir =
         warp::path::path("static")
@@ -53,7 +82,27 @@ pub async fn serve(path: PathBuf, socket_addr: SocketAddr, tls_paths: Option<(Pa
     let dist_dir = warp::path::path("dist").and(warp::fs::dir(path.join("dist")));
     let rustup_dir = warp::path::path("rustup").and(warp::fs::dir(path.join("rustup")));
     // TODO: crates_dir needs to be translated
-    let crates_dir = warp::path::path("crates").and(warp::fs::dir(path.join("crates")));
+    let crates_mirror_path = path.clone();
+    let crates_dir_native_format = warp::path!("crates" / String / String / "download").and_then(
+        move |name: String, version: String| {
+            let mirror_path = crates_mirror_path.clone();
+            async move { get_crate_file(mirror_path, &name, &version).await }
+        },
+    );
+
+    let crates_mirror_path_2 = path.clone();
+    let crates_dir_condensed_format = warp::path!("crates" / String / String).and_then(
+        move |name: String, crate_file: String| {
+            let mirror_path = crates_mirror_path_2.clone();
+            async move {
+                if !crate_file.ends_with(".crate") || !crate_file.starts_with(&name) {
+                    return Err(warp::reject::not_found());
+                }
+                let version = &crate_file[name.len() + 1..crate_file.len() - 6];
+                get_crate_file(mirror_path, &name, &version).await
+            }
+        },
+    );
 
     let git_mirror_path = path.clone();
     let git = warp::path("git")
@@ -61,12 +110,11 @@ pub async fn serve(path: PathBuf, socket_addr: SocketAddr, tls_paths: Option<(Pa
         .and(warp::path::tail())
         .and(warp::method())
         .and(warp::header::optional::<String>("Content-Type"))
-        .and(warp::header::optional::<String>("Content-Encoding"))
         .and(warp::query::raw())
         .and(warp::addr::remote())
         .and(warp::body::stream())
         .and_then(
-            move |path_tail, method, content_type, encoding, query, remote, body| {
+            move |path_tail, method, content_type, query, remote, body| {
                 let mirror_path = git_mirror_path.clone();
                 async move {
                     handle_git(
@@ -74,11 +122,11 @@ pub async fn serve(path: PathBuf, socket_addr: SocketAddr, tls_paths: Option<(Pa
                         path_tail,
                         method,
                         content_type,
-                        encoding,
                         query,
                         remote,
                         body,
-                    ).await
+                    )
+                    .await
                 }
             },
         );
@@ -91,32 +139,23 @@ pub async fn serve(path: PathBuf, socket_addr: SocketAddr, tls_paths: Option<(Pa
         .and(warp::path::tail())
         .and(warp::method())
         .and(warp::header::optional::<String>("Content-Type"))
-        .and(warp::header::optional::<String>("Content-Encoding"))
         .and(warp::addr::remote())
         .and(warp::body::stream())
-        .and_then(
-            move |path_tail, method, content_type, encoding, remote, body| {
-                let mirror_path = git_nq_mirror_path.clone();
+        .and_then(move |path_tail, method, content_type, remote, body| {
+            let mirror_path = git_nq_mirror_path.clone();
 
-                async move {
-                    handle_git_empty_query(
-                        mirror_path,
-                        path_tail,
-                        method,
-                        content_type,
-                        encoding,
-                        remote,
-                        body,
-                    ).await
-                }
-            },
-        );
+            async move {
+                handle_git_empty_query(mirror_path, path_tail, method, content_type, remote, body)
+                    .await
+            }
+        });
 
     let routes = index
         .or(static_dir)
         .or(dist_dir)
         .or(rustup_dir)
-        .or(crates_dir)
+        .or(crates_dir_native_format)
+        .or(crates_dir_condensed_format)
         .or(git)
         .or(git_no_query);
 
@@ -138,7 +177,7 @@ pub async fn serve(path: PathBuf, socket_addr: SocketAddr, tls_paths: Option<(Pa
 }
 
 /// Get all rustup platforms available on the mirror.
-async fn get_rustup_platforms(path: &Path) -> io::Result<Vec<String>> {
+async fn get_rustup_platforms(path: PathBuf) -> io::Result<Vec<(bool, String)>> {
     let rustup_path = path.join("rustup/dist");
 
     let mut output = vec![];
@@ -147,12 +186,75 @@ async fn get_rustup_platforms(path: &Path) -> io::Result<Vec<String>> {
     while let Some(entry) = rd.next_entry().await? {
         if entry.metadata().await?.is_dir() {
             if let Some(name) = entry.file_name().to_str() {
-                output.push(name.to_string());
+                let name = name.to_string();
+                if entry.path().join("rustup-init").exists() {
+                    output.push((false, name));
+                } else if entry.path().join("rustup-init.exe").exists() {
+                    output.push((true, name));
+                }
             }
         }
     }
 
+    output.sort();
+
     Ok(output)
+}
+
+/// Return a crate file as an HTTP response.
+async fn get_crate_file(
+    mirror_path: PathBuf,
+    name: &str,
+    version: &str,
+) -> Result<Response<Body>, Rejection> {
+    let crate_path = match name.len() {
+        1 => PathBuf::from("1"),
+        2 => PathBuf::from("2"),
+        3 => PathBuf::from("3"),
+        n if n >= 4 => {
+            let first_two = if let Some(first_two) = name.get(0..2) {
+                first_two
+            } else {
+                return Err(warp::reject::not_found());
+            };
+            let second_two = if let Some(second_two) = name.get(2..4) {
+                second_two
+            } else {
+                return Err(warp::reject::not_found());
+            };
+
+            [first_two, second_two].iter().collect()
+        }
+        _ => return Err(warp::reject::not_found()),
+    };
+
+    let full_path = mirror_path
+        .join("crates")
+        .join(crate_path)
+        .join(name)
+        .join(version)
+        .join(format!("{}-{}.crate", name, version));
+
+    let file = if let Ok(file) = File::open(full_path).await {
+        file
+    } else {
+        return Err(warp::reject::not_found());
+    };
+
+    let meta = if let Ok(meta) = file.metadata().await {
+        meta
+    } else {
+        return Err(warp::reject::not_found());
+    };
+    let stream = FramedRead::new(file, BytesCodec::new()).map_ok(BytesMut::freeze);
+
+    let body = Body::wrap_stream(stream);
+
+    let mut resp = Response::new(body);
+    resp.headers_mut()
+        .insert(http::header::CONTENT_LENGTH, meta.len().into());
+
+    Ok(resp)
 }
 
 /// Handle a request from a git client.
@@ -162,7 +264,6 @@ async fn handle_git_empty_query<S, B>(
     path_tail: Tail,
     method: http::Method,
     content_type: Option<String>,
-    encoding: Option<String>,
     remote: Option<SocketAddr>,
     body: S,
 ) -> Result<Response<Body>, Rejection>
@@ -175,7 +276,6 @@ where
         path_tail,
         method,
         content_type,
-        encoding,
         String::new(),
         remote,
         body,
@@ -189,7 +289,6 @@ async fn handle_git<S, B>(
     path_tail: Tail,
     method: http::Method,
     content_type: Option<String>,
-    encoding: Option<String>,
     query: String,
     remote: Option<SocketAddr>,
     mut body: S,
@@ -198,21 +297,16 @@ where
     S: Stream<Item = Result<B, warp::Error>> + Send + Unpin + 'static,
     B: bytes::Buf + Sized,
 {
-    dbg!(
-        &path_tail,
-        &method,
-        &content_type,
-        &encoding,
-        &query,
-        &remote
-    );
-
     let remote = remote
         .map(|r| r.ip().to_string())
         .unwrap_or_else(|| "127.0.0.1".to_string());
 
+    // Run "git http-backend"
     let mut cmd = Command::new("git");
     cmd.arg("http-backend");
+
+    // Clear environment variables, and set needed variables
+    // See: https://git-scm.com/docs/git-http-backend
     cmd.env_clear();
     cmd.env("GIT_PROJECT_ROOT", mirror_path);
     cmd.env(
@@ -231,21 +325,27 @@ where
     cmd.stdout(Stdio::piped());
     cmd.stdin(Stdio::piped());
 
-    let p = cmd.spawn().unwrap();
+    let p = cmd.spawn().map_err(|e| ServeError::from(e))?;
 
-    let mut git_input = p.stdin.unwrap();
-    // Handle sending body to http-backend, if any
+    // Handle sending git client body to http-backend, if any
+    let mut git_input = p.stdin.expect("Process should always have stdin");
     while let Some(Ok(mut buf)) = body.next().await {
-        git_input.write_all_buf(&mut buf).await.unwrap();
+        git_input
+            .write_all_buf(&mut buf)
+            .await
+            .map_err(|e| ServeError::from(e))?;
     }
 
-    let mut git_output = BufReader::new(p.stdout.unwrap());
+    let mut git_output = BufReader::new(p.stdout.expect("Process should always have stdout"));
 
     // Collect headers from git CGI output
     let mut headers = HashMap::new();
     loop {
         let mut line = String::new();
-        git_output.read_line(&mut line).await.unwrap();
+        git_output
+            .read_line(&mut line)
+            .await
+            .map_err(|e| ServeError::from(e))?;
 
         let line = line.trim_end();
         if line.is_empty() {
@@ -256,7 +356,6 @@ where
             headers.insert(key.to_string(), value.to_string());
         }
     }
-    dbg!(&headers);
 
     // Add headers to response (except for Status, which is the "200 OK" line)
     let mut resp = Response::builder();
@@ -273,19 +372,22 @@ where
     let (sender, body) = Body::channel();
     tokio::spawn(send_git(sender, git_output));
 
-    let resp = resp.body(body).unwrap();
+    let resp = resp.body(body).map_err(|e| ServeError::from(e))?;
     Ok(resp)
 }
 
 /// Send data from git CGI process to hyper Sender, until there is no more
 /// data left.
-async fn send_git(mut sender: Sender, mut git_output: BufReader<ChildStdout>) {
+async fn send_git(
+    mut sender: Sender,
+    mut git_output: BufReader<ChildStdout>,
+) -> Result<(), ServeError> {
     loop {
         let mut bytes_out = BytesMut::new();
-        git_output.read_buf(&mut bytes_out).await.unwrap();
+        git_output.read_buf(&mut bytes_out).await?;
         if bytes_out.is_empty() {
-            return;
+            return Ok(());
         }
-        sender.send_data(bytes_out.freeze()).await.unwrap();
+        sender.send_data(bytes_out.freeze()).await?;
     }
 }
